1bfc0460f993fa49deb6f8f5ffa1e5cf24d183b2
thanhd <thanhdanh@icd-vn.com>
2023-11-10 21:08:39 +0700

    update: remove cy test

--- packages/frontend/src/components/main-dashboard/card/GridThird.cy.tsx
+++
@@ -1,8 +0,0 @@
-import GridThird from './GridThird';
-
-describe('<GridThird />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<GridThird />);
-  });
-});
